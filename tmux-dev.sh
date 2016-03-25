d0a5c2fda12ca282fe6641e877816d3203422f55
Jeremy Winterberg <winterjd@uwec.edu>
2016-03-24 22:54:19 -0500

    needed to detach from the tmux session to run the pane split commands

--- tmux-dev.sh
+++ tmux-dev.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-source-file ~/.tmux.conf
-new-session -s dev -n ide
-split-window -v -p 10 -t dev
-select-pane -t 1
-split-window -h -p 30 -t dev
-new-window -n shell
-select-window -t dev:1
-select-pane -t 1
+tmux new-session -d -s dev -n ide
+tmux split-window -v -p 10 -t dev
+tmux select-pane -t 1
+tmux split-window -h -p 30 -t dev
+tmux new-window -n shell
+tmux select-window -t dev:1
+tmux select-pane -t 1
+tmux -2 attach-session -t dev
