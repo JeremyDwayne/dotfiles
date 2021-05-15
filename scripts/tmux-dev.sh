61788f4b3634c045c70104937aa9b9cdad4e3b27
Jeremy Winterberg <jeremy@jeremydwayne.com>
2021-05-14 23:07:18 -0500

    refactor: changed tdev script for easier TDD

--- scripts/tmux-dev.sh
+++ scripts/tmux-dev.sh
@@ -16,7 +16,8 @@ tmux set-option default-path "$PWD"
 # Creates session, and names window DEV
 tmux new-session -d -x "$(tput cols)" -y "$(tput lines)" -s $1 -c $2
 tmux rename-window 'DEV'
-tmux split-window -v -p 20 -c $2
+tmux split-window -h -p 50 -c $2
+tmux split-window -vf -p 20 -c $2
 
 # Creates second window named SERVER
 tmux new-window -a -d -n 'SERVER' -c $2
