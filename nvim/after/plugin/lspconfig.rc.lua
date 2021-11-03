23a17e4f52910b1856c4b73e9704ca58ca798b01
Jeremy Winterberg <jeremy@jeremydwayne.com>
2021-11-03 09:26:09 -0500

    add json lsp server

--- nvim/after/plugin/lspconfig.rc.lua
+++ nvim/after/plugin/lspconfig.rc.lua
@@ -305,4 +305,16 @@
               }
           }
       }
+
   }
+
+  require'lspconfig'.jsonls.setup {
+    commands = {
+      Format = {
+        function()
+          vim.lsp.buf.range_formatting({},{0,0},{vim.fn.line("$"),0})
+        end
+      }
+    }
+  }
+
