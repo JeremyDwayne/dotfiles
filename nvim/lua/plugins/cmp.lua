4c7556c0db0b88bf591b4db5d2ac69acf62ff89e
Jeremy Winterberg <jeremy@jeremydwayne.com>
2023-06-27 23:19:12 -0500

    copilot working again

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,40 @@
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+end
+return {
+  'hrsh7th/nvim-cmp',
+  config = function()
+    local cmp = require('cmp')
+    local lspkind = require('lspkind')
+
+    cmp.setup({
+      sources = {
+        -- Copilot Source
+        { name = "copilot",  group_index = 2 },
+        -- Other Sources
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "path",     group_index = 2 },
+        { name = "luasnip",  group_index = 2 },
+      },
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = "symbol",
+          max_width = 50,
+          symbol_map = { Copilot = "" }
+        })
+      },
+      mapping = {
+        ["<Tab>"] = vim.schedule_wrap(function(fallback)
+          if cmp.visible() and has_words_before() then
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+          else
+            fallback()
+          end
+        end),
+      }
+    }
+    )
+  end
+}
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -115,9 +115,13 @@ return {
   },
   {
     "zbirenbaum/copilot.lua",
-    lazy = false,
+    cmd = "Copilot",
+    event = "InsertEnter",
     config = function()
-      require("copilot").setup()
+      require("copilot").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      })
     end,
   },
   {
