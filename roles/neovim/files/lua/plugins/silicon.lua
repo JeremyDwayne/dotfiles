0f7931bc3622bc363e0c38d47906c0bfd6bcd0b4
JeremyDwayne <jeremy@jeremydwayne.com>
2024-02-29 16:18:37 -0600

    switched back to filename

--- roles/neovim/files/lua/plugins/silicon.lua
+++ roles/neovim/files/lua/plugins/silicon.lua
@@ -8,11 +8,9 @@ return {
       font = "Dank Mono=34;Noto Color Emoji=34",
       theme = "Dracula",
       background = "#94e2d5",
-      window_title = "jeremywinterberg.com",
-      -- File name for window title
-      -- function()
-      --   return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
-      -- end,
+      window_title = function()
+        return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
+      end,
       to_clipboard = true,
     })
   end,
