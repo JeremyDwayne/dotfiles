574337e8ff3f4dfabea290b17faaafdff4df0ed7
JeremyDwayne <jeremy@jeremydwayne.com>
2024-02-29 16:17:17 -0600

    added basic assets, added silicon screenshot nvim

---
+++ roles/neovim/files/lua/plugins/silicon.lua
@@ -0,0 +1,19 @@
+return {
+  "michaelrommel/nvim-silicon",
+  lazy = true,
+  cmd = "Silicon",
+  config = function()
+    require("silicon").setup({
+      -- Configuration here, or leave empty to use defaults
+      font = "Dank Mono=34;Noto Color Emoji=34",
+      theme = "Dracula",
+      background = "#94e2d5",
+      window_title = "jeremywinterberg.com",
+      -- File name for window title
+      -- function()
+      --   return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
+      -- end,
+      to_clipboard = true,
+    })
+  end,
+}
