63bd5ca74b76f16cb0e63555ae686d1f0a9d046c
JeremyDwayne <jeremy@jeremydwayne.com>
2024-03-03 14:09:12 -0600

    changed icons for pomodoro

--- roles/tmux/files/tmux/custom/pomodoro.sh
+++ roles/tmux/files/tmux/custom/pomodoro.sh
@@ -1,7 +1,7 @@
 show_pomodoro() {
 	local index=$1
-	local icon=$(get_tmux_option "@catppuccin_pomodoro_icon" "🍅")
-	local color=$(get_tmux_option "@catppuccin_pomodoro_color" "$thm_green")
+	local icon=$(get_tmux_option "@catppuccin_pomodoro_icon" "")
+	local color=$(get_tmux_option "@catppuccin_pomodoro_color" "$thm_red")
 	local text=$(get_tmux_option "@catppuccin_pomodoro_text" "#{pomodoro_status}")
 
 	local module=$(build_status_module "$index" "$icon" "$color" "$text")
