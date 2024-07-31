fb02357f2cf2566ed6bae0fe3da167571c1ba93f
JeremyDwayne <jeremy@jeremydwayne.com>
2024-07-31 18:37:34 -0400

    remove direnv for now

--- roles/zsh/files/zsh/plugins.zsh
+++ roles/zsh/files/zsh/plugins.zsh
@@ -8,3 +8,4 @@ if type starship &>/dev/null; then
 fi
 
 source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
