114fbb08927947896eceae3252bb991094f18dd4
JeremyDwayne <jeremy@jeremydwayne.com>
2025-01-20 14:36:37 -0500

    remove copilot, remove deprecated obsidian config

--- roles/neovim/files/lua/plugins/obsidian.lua
+++ roles/neovim/files/lua/plugins/obsidian.lua
@@ -34,7 +34,7 @@ return {
     },
     completion = {
       -- Set to false to disable completion.
-      nvim_cmp = true,
+      -- nvim_cmp = true,
 
       -- Trigger completion at 2 chars.
       min_chars = 2,
