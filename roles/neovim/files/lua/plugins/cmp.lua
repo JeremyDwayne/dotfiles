e993f94fbadbf0a6780de518312a11b11b7cef36
JeremyDwayne <jeremy@jeremydwayne.com>
2025-04-10 22:22:24 -0400

    `chore: Remove unused Neovim plugins and update plugin configuration`

--- roles/neovim/files/lua/plugins/cmp.lua
+++ roles/neovim/files/lua/plugins/cmp.lua
@@ -25,27 +25,4 @@ return {
       }),
     })
   end,
-  -- config = function()
-  --   local cmp = require("cmp")
-  --   cmp.setup.cmdline("/", {
-  --     mapping = cmp.mapping.preset.cmdline(),
-  --     sources = {
-  --       { name = "buffer" },
-  --     },
-  --   })
-  --
-  --   cmp.setup.cmdline(":", {
-  --     mapping = cmp.mapping.preset.cmdline(),
-  --     sources = cmp.config.sources({
-  --       { name = "path" },
-  --     }, {
-  --       {
-  --         name = "cmdline",
-  --         option = {
-  --           ignore_cmds = { "Man", "!" },
-  --         },
-  --       },
-  --     }),
-  --   })
-  -- end,
 }
--- roles/neovim/files/lua/plugins/disabled.lua
+++ roles/neovim/files/lua/plugins/disabled.lua
@@ -2,5 +2,6 @@ return {
   { "folke/flash.nvim", enabled = false },
   { "folke/which-key.nvim", enabled = false },
   { "echasnovski/mini.ai", enabled = false },
+  { "jose-elias-alvarez/null-ls.nvim", enabled = false },
   -- { "mini.pairs", enabled = false },
 }
